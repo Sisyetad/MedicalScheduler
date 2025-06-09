2e7f1a50cffb45f8ddaff0807d1801b62d764609
Sisay <sisaytadewos@gmail.com>
2025-06-09 11:03:04 +0300

    edit home_page

--- medical_scheduler/lib/home_page.dart
+++ medical_scheduler/lib/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:medical_scheduler/presentation/Provider/providers/auth_provider.dart';
+import 'package:medical_scheduler/presentation/Provider/providers/Auth/auth_provider.dart';
 
 class SplashPage extends ConsumerStatefulWidget {
   const SplashPage({super.key});
@@ -14,14 +14,13 @@ class _SplashPageState extends ConsumerState<SplashPage> {
   final storage = const FlutterSecureStorage();
 
   @override
-void initState() {
-  super.initState();
-  Future.microtask(() {
-    final authViewModel = ref.read(authViewModelProvider.notifier);
-    authViewModel.checkLoginStatus(context); 
-  });
-}
-
+  void initState() {
+    super.initState();
+    Future.microtask(() {
+      final authViewModel = ref.read(authViewModelProvider.notifier);
+      authViewModel.checkLoginStatus(context);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
