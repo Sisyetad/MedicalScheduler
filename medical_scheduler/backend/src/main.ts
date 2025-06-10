494ada6ba78f6b323f4d77fed910318b05370259
DagmawiKK <dagmawikassa1@gmail.com>
2025-06-10 20:38:54 +0300

    add backend CORS Policy

--- medical_scheduler/backend/src/main.ts
+++ medical_scheduler/backend/src/main.ts
@@ -7,7 +7,12 @@ async function bootstrap() {
   app.setGlobalPrefix('api'); // localhost/api
   app.use(cookieParser());
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://127.0.0.1:5500'], //for React
+    origin: [
+      'http://localhost:3000',
+      'http://127.0.0.1:5500',
+      'http://localhost:35053',
+      'http://localhost:44667',
+    ], //for React
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 4000);
