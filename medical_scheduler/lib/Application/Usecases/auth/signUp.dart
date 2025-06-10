e2296a4866b82e376dbc4c19a64f4640d01ea050
saron03 <saronyitbe@gmail.com>
2025-06-10 17:15:27 +0300

    Add signUp logic

--- medical_scheduler/lib/Application/Usecases/auth/signUp.dart
+++ medical_scheduler/lib/Application/Usecases/auth/signUp.dart
@@ -1,1 +1,22 @@
 // domain/usecases/auth/register_user.dart
+
+import 'package:medical_scheduler/core/usecases/base_useacase.dart';
+import 'package:medical_scheduler/core/usecases/params.dart';
+import 'package:medical_scheduler/domain/entities/response/authresponse.dart';
+import 'package:medical_scheduler/domain/repository/autho_repo.dart';
+
+class RegisterUseCase extends UseCase<AuthResponse, RegisterParams> {
+  final AuthRepository repository;
+
+  RegisterUseCase(this.repository);
+
+  @override
+  Future<AuthResponse> call(RegisterParams params) {
+    return repository.register(
+      params.signupRequest.name,
+      params.signupRequest.email,
+      params.signupRequest.password,
+      params.signupRequest.role
+    );
+  }
+}
