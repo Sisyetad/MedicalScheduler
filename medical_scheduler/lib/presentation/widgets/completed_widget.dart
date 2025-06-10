6d8f58abcb3b0c8e540e9cd8f26692c8f2039577
DagmawiKK <dagmawikassa1@gmail.com>
2025-06-10 20:34:47 +0300

    update completed page

--- medical_scheduler/lib/presentation/widgets/completed_widget.dart
+++ medical_scheduler/lib/presentation/widgets/completed_widget.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class Completed extends StatelessWidget {
   final int completedCount;
-  const Completed({super.key, required this.completedCount});
+  const Completed({super.key, required this.completedCount, required String label});
 
   @override
   Widget build(BuildContext context) {
