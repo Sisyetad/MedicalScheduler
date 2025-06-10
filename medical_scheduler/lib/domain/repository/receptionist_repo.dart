e9a623965bafcb3ca437f59c2b59c9b325f8fb49
BruhTes <bruhtesfahun21129@gmail.com>
2025-06-10 12:07:19 +0300

    repository for add doctor

--- medical_scheduler/lib/domain/repository/receptionist_repo.dart
+++ medical_scheduler/lib/domain/repository/receptionist_repo.dart
@@ -1,10 +1,10 @@
-import 'package:medical_scheduler/domain/entities/request/receptionist_request.dart';
+import 'package:medical_scheduler/domain/entities/request/doctor_request.dart';
 import 'package:medical_scheduler/domain/entities/response/receptionist.dart';
 
 abstract class ReceptionistRepository {
   Future<List<Receptionist>> getAllReceptionists();
   Future<Receptionist?> getReceptionistById(int receptionistId);
-  Future<Receptionist> createReceptionist(ReceptionistRequest receptionist);
+  Future<Receptionist> createReceptionist(EmployeeRequest receptionist);
   Future<void> updateReceptionist(Receptionist receptionist);
   Future<void> deleteReceptionist(int id);
 }
